8faed9ffa826dc48abc02b76dad1fd88df008824
Neill-Tsou <1317797708@qq.com>
2022-08-16 10:07:41 +0800

    COS UPDATED

--- src/views/upload/index.vue
+++ src/views/upload/index.vue
@@ -2,7 +2,7 @@
     <!-- Head -->
     <van-nav-bar title="分享图片故事" left-text="返回" left-arrow @click-left="onClickLeft" />
     <!-- Uploader -->
-    <van-uploader v-model="fileList" multiple :after-read="afterRead" />
+    <van-uploader v-model="fileList" multiple :after-read="afterRead" accept='image/*' max-size="5242880" />
 
 </template>
 
@@ -17,43 +17,47 @@ export default {
         'van-nav-bar': NavBar
     },
     setup() {
-        const fileList = ref([
-            { url: 'https://fastly.jsdelivr.net/npm/@vant/assets/leaf.jpeg' },
-        ]);
+        //拿到图片url，预览
+        const fileList = ref([]);
+        let pid = 0;
         const afterRead = (file) => {
             // 此时可以自行将文件上传至服务器
             console.log(file);
+            //上传前行为，记录这一次上传的uid
+            // this.currentFileUid = file.uid;
+            //上传行为，创建COS实例
             var cos = new COS({
                 SecretId: 'AKIDbCfv23ua6RfoDRUMH9qKhtDQYYmqjVBt',
                 SecretKey: 'kMqLhIG5lJm4eHj7UtIYmuIbl4ENMBtu',
             });
+            //存储图片键名
+            pid += 1
+            //调用对象存储方法
             cos.putObject(
                 {
                     Bucket: "bucket-1313109230" /* 填入您自己的存储桶，必须字段 */,
                     Region: "ap-chongqing" /* 存储桶所在地域，例如ap-beijing，必须字段 */,
-                    Key: file.file.name /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */,
+                    Key: `pid${pid}` /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */,
                     StorageClass: "STANDARD",
                     Body: file.file, // 上传文件对象
                 },
                 (err, data) => {
                     if (err) {
-                        console.log("上传失败", err);
+                        console.log(err);
+                        Toast.fail("上传失败")
                     } else {
                         Toast.success('上传成功')
-                        this.fileMenu.push(data.Location)
-                        console.log(fileMenu)
+                        console.log(fileList)
                     }
                 }
-
             )
         };
         const onClickLeft = () => history.back();
 
         return {
             fileList,
-            fileMenu: [],
             onClickLeft,
-            afterRead
+            afterRead,
         };
     }
 
